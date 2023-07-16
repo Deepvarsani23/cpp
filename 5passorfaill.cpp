74db5f91e0bbe964053aadf91455cf84e423e1b7
Deepvarsani23 <deepvarsani2305@gmail.com>
2023-07-16 14:47:14 +0530

    16-7-2023

---
+++ 5passorfaill.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a value : ";
+    cin>>a;
+    if(a>32)
+    {
+        cout<<a<<" student is pass";
+    }
+    else
+    {
+        cout<<a<<" student is faill";
+    }
+ }
---
+++ 6leapyear.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter a value : ";
+    cin>>a;
+    if(a%4==0)
+    {
+        cout<<a<<" is leap year";
+    }
+    else
+    {
+        cout<<a<<" is not leap year";
+    }
+}
